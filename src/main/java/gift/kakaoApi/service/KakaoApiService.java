0949d4f1b3ff5eb9738cd32c5ab09e7608ff6e58
Kangboom <jkl6147@naver.com>
2024-07-28 21:18:26 +0900

    refactor: Change constant declarations to use properties values

--- src/main/java/gift/kakaoApi/service/KakaoApiService.java
+++ src/main/java/gift/kakaoApi/service/KakaoApiService.java
@@ -2,13 +2,11 @@ package gift.kakaoApi.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import gift.domain.order.entity.Orders;
-import gift.kakaoApi.dto.token.KakaoTokenFailResponse;
+import gift.kakaoApi.dto.message.Link;
+import gift.kakaoApi.dto.message.TemplateObject;
 import gift.kakaoApi.dto.token.KakaoTokenResponse;
 import gift.kakaoApi.dto.userInfo.KakaoUserInfoResponse;
-import gift.kakaoApi.dto.message.Link;
-import gift.kakaoApi.dto.message.MessageResponse;
-import gift.kakaoApi.dto.message.TemplateObject;
-import gift.kakaoApi.exceptiion.KakaoLoginException;
+import gift.kakaoApi.exception.KakaoLoginException;
 import gift.kakaoApi.properties.KakaoProperties;
 import java.net.URI;
 import org.springframework.http.HttpHeaders;
@@ -20,12 +18,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 @Service
 public class KakaoApiService {
-
-    public static final String BEARER_PREPIX = "Bearer ";
-    private static final String KAKAO_AUTHORIZATION_CODE_URL = "https://kauth.kakao.com/oauth/authorize";
-    private static final String KAKAO_TOEKN_URL = "https://kauth.kakao.com/oauth/token";
-    private static final String KAKAO_ACCOUNT_URL = "https://kapi.kakao.com/v2/user/me";
-    private static final String KAKAO_SEND_MESSAGE_URL = "https://kapi.kakao.com/v2/api/talk/memo/default/send";
+    public static final String BEARER_PREFIX = "Bearer ";
     private final KakaoProperties properties;
     private final ObjectMapper objectMapper;
     private final RestClient client;
@@ -39,7 +32,7 @@ public class KakaoApiService {
 
     public String getKakaoLoginUri() {
         return UriComponentsBuilder.newInstance()
-            .path(KAKAO_AUTHORIZATION_CODE_URL)
+            .path(properties.authorizationCodeUrl())
             .queryParam("response_type", "code")
             .queryParam("client_id", properties.clientId())
             .queryParam("redirect_uri", properties.redirectUrl())
@@ -50,15 +43,12 @@ public class KakaoApiService {
         LinkedMultiValueMap<String, String> body = creatBody(code);
 
         return client.post()
-            .uri(URI.create(KAKAO_TOEKN_URL))
+            .uri(URI.create(properties.tokenUrl()))
             .contentType(MediaType.APPLICATION_FORM_URLENCODED)
             .body(body)
             .exchange((request, response) -> {
-                if (response.getStatusCode().is4xxClientError()) {
-                    KakaoTokenFailResponse failResponse = objectMapper.readValue(response.getBody(),
-                        KakaoTokenFailResponse.class);
-                    throw new KakaoLoginException(failResponse.errorCode(),
-                        failResponse.errorDescription());
+                if (!response.getStatusCode().is2xxSuccessful()) {
+                    throw new KakaoLoginException("카카오 인증 실패");
                 }
 
                 return objectMapper.readValue(response.getBody(), KakaoTokenResponse.class);
@@ -67,34 +57,40 @@ public class KakaoApiService {
 
     public KakaoUserInfoResponse getKakaoAccount(String accessToken) {
         return client.get()
-            .uri(URI.create(KAKAO_ACCOUNT_URL))
-            .header(HttpHeaders.AUTHORIZATION, BEARER_PREPIX + accessToken)
+            .uri(URI.create(properties.accountUrl()))
+            .header(HttpHeaders.AUTHORIZATION, BEARER_PREFIX + accessToken)
             .retrieve()
             .body(KakaoUserInfoResponse.class);
     }
 
-    public MessageResponse sendKakaoMessage(String accessToken, Orders order) {
+    public Boolean sendKakaoMessage(String accessToken, Orders order) {
         String orderMessage = createOrderMessage(order);
         Link link = new Link("http://localhost:8080", "http://localhost:8080");
         TemplateObject templateObject = new TemplateObject("text", orderMessage, link);
 
         return client.post()
-            .uri(URI.create(KAKAO_SEND_MESSAGE_URL))
+            .uri(URI.create(properties.sendMessageUrl()))
             .contentType(MediaType.APPLICATION_FORM_URLENCODED)
-            .header(HttpHeaders.AUTHORIZATION, BEARER_PREPIX + accessToken)
+            .header(HttpHeaders.AUTHORIZATION, BEARER_PREFIX + accessToken)
             .body(templateObject.toRequestBody())
-            .retrieve()
-            .body(MessageResponse.class);
+            .exchange((request, response) -> {
+                if (response.getStatusCode().is2xxSuccessful()) {
+                    return true;
+                }
+                return false;
+            });
     }
 
     public String createOrderMessage(Orders order) {
-        String messageTemplate = "[상품 주문]\n"
-            + "상품명: %s\n"
-            + "옵션\n"
+        String messageTemplate = "[주문 내역]\n"
+            + "\n[상품]\n"
+            + "  상품명: %s\n"
+            + "\n[옵션]\n"
             + "  옵션명: %s\n"
             + "  수량: %d\n"
-            + "요청사항: %s\n"
-            + "상품 주문이 완료되었습니다.";
+            + "\n[요청사항]\n"
+            + "  요청 메세지: %s\n"
+            + "\n상품 주문이 완료되었습니다.\n";
 
         String productName = order.getOption().getProduct().getName();
         String optionName = order.getOption().getName();
