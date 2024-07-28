12288ef1b14240e8f3c262062f647d9b46dd0e05
Kangboom <jkl6147@naver.com>
2024-07-28 20:59:50 +0900

    refactor: Change exception inheritance

--- src/main/java/gift/kakaoApi/exceptiion/KakaoMessageException.java
+++
@@ -1,8 +0,0 @@
-package gift.kakaoApi.exceptiion;
-
-public class KakaoMessageException extends RuntimeException {
-
-    public KakaoMessageException(String message) {
-        super(message);
-    }
-}
---
+++ src/main/java/gift/kakaoApi/exception/KakaoMessageException.java
@@ -0,0 +1,7 @@
+package gift.kakaoApi.exception;
+
+public class KakaoMessageException extends KakaoApiException {
+    public KakaoMessageException(String message) {
+        super(message);
+    }
+}
