210394ab160bebf11cd26f21952376c674c2455c
Kangboom <jkl6147@naver.com>
2024-07-28 21:02:03 +0900

    feat: Add Kakao Api URL

--- src/main/java/gift/kakaoApi/properties/KakaoProperties.java
+++ src/main/java/gift/kakaoApi/properties/KakaoProperties.java
@@ -5,7 +5,11 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "kakao")
 public record KakaoProperties(
     String clientId,
-    String redirectUrl
+    String redirectUrl,
+    String authorizationCodeUrl,
+    String tokenUrl,
+    String accountUrl,
+    String sendMessageUrl
 ) {
 
 }
