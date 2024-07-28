2d1d50ca91c2b474c1f5ecaf188aaf4bc2626810
Kangboom <jkl6147@naver.com>
2024-07-28 21:05:23 +0900

    feat: Add Swagger configuration

--- src/main/java/gift/domain/category/dto/CategoryRequest.java
+++ src/main/java/gift/domain/category/dto/CategoryRequest.java
@@ -1,13 +1,19 @@
 package gift.domain.category.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "카테고리 요청 DTO")
 public class CategoryRequest {
 
+    @Schema(description = "카테고리 이름", example = "카테고리 1")
     private String name;
-
+    @Schema(description = "카테고리 색깔", example = "blue")
     private String color;
 
+    @Schema(description = "카테고리 이미지 URL", example = "http://example.com/image.jpg")
     private String imageUrl;
 
+    @Schema(description = "카테고리 설명", example = "설명")
     private String description;
 
     private CategoryRequest() {
