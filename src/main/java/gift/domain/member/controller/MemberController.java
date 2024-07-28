b126c9265d2f4b69681152c85d05b128b164e29b
Kangboom <jkl6147@naver.com>
2024-07-28 20:56:03 +0900

    feat: Add Swagger configuration

--- src/main/java/gift/domain/member/controller/MemberController.java
+++ src/main/java/gift/domain/member/controller/MemberController.java
@@ -4,7 +4,12 @@ import gift.domain.member.dto.MemberRequest;
 import gift.domain.member.dto.MemberResponse;
 import gift.domain.member.service.MemberService;
 import gift.util.dto.JwtResponse;
-import java.util.List;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/members")
+@Tag(name = "MemberController", description = "Member API")
 public class MemberController {
 
     private final MemberService memberService;
@@ -28,6 +34,12 @@ public class MemberController {
     }
 
     @GetMapping()
+    @Operation(summary = "전체 유저 조회", description = "전체 유저를 조회합니다.")
+    @ApiResponse(responseCode = "200", description = "요청에 성공하였습니다.", content = @Content(mediaType = "application/json"))
+    @Parameters({
+        @Parameter(name = "pageNo", description = "페이지 번호 (0부터 시작)", example = "0"),
+        @Parameter(name = "pageSize", description = "페이지 크기", example = "10")
+    })
     public ResponseEntity<Page<MemberResponse>> getAllMember(
         @RequestParam(defaultValue = "0") int pageNo,
         @RequestParam(defaultValue = "10") int pageSize
@@ -37,6 +49,8 @@ public class MemberController {
     }
 
     @PostMapping("/register")
+    @Operation(summary = "회원 가입", description = "회원 가입을 요청합니다.")
+    @ApiResponse(responseCode = "201", description = "요청에 성공하였습니다.", content = @Content(mediaType = "application/json"))
     public ResponseEntity<JwtResponse> register(@RequestBody MemberRequest memberRequest) {
         String token = memberService.register(memberRequest);
 
@@ -45,6 +59,8 @@ public class MemberController {
     }
 
     @PostMapping("/login")
+    @Operation(summary = "로그인", description = "로그인을 진행합니다.")
+    @ApiResponse(responseCode = "200", description = "요청에 성공하였습니다.", content = @Content(mediaType = "application/json"))
     public ResponseEntity<JwtResponse> login(@RequestBody MemberRequest memberRequest) {
 
         String token = memberService.login(memberRequest);
@@ -56,6 +72,9 @@ public class MemberController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "회원 삭제", description = "저장된 회원을 삭제합니다.")
+    @ApiResponse(responseCode = "204", description = "요청에 성공하였습니다.")
+    @Parameter(name = "id", description = "삭제할 회원의 Id")
     public ResponseEntity<Void> delete(@PathVariable("id") Long id) {
         memberService.deleteMember(id);
         return ResponseEntity.noContent().build();
--- src/main/java/gift/domain/wishlist/controller/WishController.java
+++ src/main/java/gift/domain/wishlist/controller/WishController.java
@@ -6,15 +6,27 @@ import gift.domain.wishlist.dto.ProductIdRequest;
 import gift.domain.wishlist.dto.WishRequest;
 import gift.domain.wishlist.dto.WishResponse;
 import gift.domain.wishlist.service.WishService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/wishes")
+@Tag(name = "WishController", description = "WishController API(JWT 인증 필요)")
 public class WishController {
 
     private final WishService wishService;
@@ -24,8 +36,14 @@ public class WishController {
     }
 
     @GetMapping
+    @Operation(summary = "전체 위시리스트 조회", description = "전체 위시리스트 조회합니다.")
+    @ApiResponse(responseCode = "200", description = "요청에 성공하였습니다.", content = @Content(mediaType = "application/json"))
+    @Parameters({
+        @Parameter(name = "pageNo", description = "페이지 번호 (0부터 시작)", example = "0"),
+        @Parameter(name = "pageSize", description = "페이지 크기", example = "10")
+    })
     public ResponseEntity<Page<WishResponse>> getWishes(
-        @LoginMember Member member,
+        @Parameter(hidden = true) @LoginMember Member member,
         @RequestParam(defaultValue = "0") int pageNo,
         @RequestParam(defaultValue = "10") int pageSize
     ) {
@@ -34,8 +52,10 @@ public class WishController {
     }
 
     @PostMapping
+    @Operation(summary = "위시리스트 생성", description = "위시 리스트를 생성합니다.")
+    @ApiResponse(responseCode = "201", description = "요청에 성공하였습니다.", content = @Content(mediaType = "application/json"))
     public ResponseEntity<WishResponse> createWish(@RequestBody ProductIdRequest productIdRequest,
-        @LoginMember Member member) {
+        @Parameter(hidden = true) @LoginMember Member member) {
         WishRequest wishRequest = new WishRequest(member.getId(), productIdRequest.getProductId());
         WishResponse response = wishService.createWish(wishRequest);
         return ResponseEntity.status(HttpStatus.CREATED)
@@ -44,8 +64,11 @@ public class WishController {
 
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "위시 리스트 삭제", description = "해당 위시 리스트를 삭제합니다.")
+    @ApiResponse(responseCode = "204", description = "요청에 성공하였습니다.")
+    @Parameter(name = "id", description = "삭제할 위시 리스트의 ID", example = "1")
     public ResponseEntity<WishResponse> deleteWish(@PathVariable("id") Long id,
-        @LoginMember Member member) {
+        @Parameter(hidden = true) @LoginMember Member member) {
         wishService.deleteWish(id, member);
 
         return ResponseEntity.noContent().build();
